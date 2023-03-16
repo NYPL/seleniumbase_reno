56f9b4c57e51dab58e7d1b08eaeabfab6da2bacb
alkimcevik <alkimcevik@nypl.org>
2023-03-16 11:42:37 -0400

    give page added

--- examples/nypl_pages/page_schwarzman.py
+++ examples/nypl_pages/page_schwarzman.py
@@ -2,7 +2,6 @@ from seleniumbase import BaseCase
 
 
 class SchwarzmanPage(BaseCase):
-
     home = '//*[@id="block-nypl-emulsify-breadcrumbs"]/nav/ul/li[1]/a'
     locations = '//*[@id="block-nypl-emulsify-breadcrumbs"]/nav/ul/li[2]/a'
     hero = '//*[@id="block-content-hero-header"]/div/div[2]/h1/span'
@@ -11,7 +10,27 @@ class SchwarzmanPage(BaseCase):
     research = '//*[@id="audience-navigation--"]/li[2]/a'
     directions = '//*[@id="block-entityviewcontent"]/div/div/div/a'
     holiday_closings = '//*[@id="block-entityviewcontent"]/div/div/div/div[5]/a'
-    learn_more_link = '//*[@id="block-nypl-emulsify-content"]/div/div/div[1]/div/p[2]/a'
+    learn_more_1 = '//*[@id="block-nypl-emulsify-content"]/div/div/div[1]/div/p[2]/a'
+    learn_more_2 = '//*[@id="block-nypl-emulsify-content"]/div/div/div[2]/div[2]/p/a'
+    daily_guided_tours = '//*[@id="block-nypl-emulsify-content"]/div/div/div[3]/div[2]/p/a'
+
+    in_the_spotlight_1 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[4]/ul/li[1]/div[1]'
+    in_the_spotlight_2 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[4]/ul/li[2]/div[1]'
+
+    featured_at_sasb_1 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[5]/ul/li[1]/div[1]'
+    featured_at_sasb_2 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[5]/ul/li[2]/div[1]'
+    featured_at_sasb_3 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[5]/ul/li[3]/div[1]'
+
+    current_exhibitions_list = ('//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[1]/div/div/div[2]/ul/li')
+
+    current_exhibitions_1 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[6]/div[1]/div/div/div[2]/ul/li[1]'
+    current_exhibitions_2 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[6]/div[1]/div/div/div[2]/ul/li[2]'
+    current_exhibitions_3 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[6]/div[1]/div/div/div[2]/ul/li[3]'
+    current_exhibitions_4 = '/html/body/div[1]/div/main/div[3]/div/div[1]/div/div/div[6]/div[1]/div/div/div[2]/ul/li[4]'
+
+    events_see_all = '//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[2]/div/div/div/div/a'
+
+    about_the_sasb = '//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[3]/h2'
 
     def open_schwarzman_page(self):
         # self.open("https://www.nypl.org/locations/schwarzman")
--- examples/nypl_tests/test_schwarzman.py
+++ examples/nypl_tests/test_schwarzman.py
@@ -1,6 +1,13 @@
 from examples.nypl_pages.page_schwarzman import SchwarzmanPage
 from examples.nypl_utility.utility import NyplUtils
 
+import requests
+import urllib3
+import pytest
+from requests.exceptions import MissingSchema, InvalidSchema, InvalidURL
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class Schwarzman(NyplUtils):
 
@@ -20,7 +27,6 @@ class Schwarzman(NyplUtils):
         super().tearDown()
 
     def test_schwarzman(self):
-
         # https://www.nypl.org/locations/schwarzman
         print("test_schwarzman()\n")
 
@@ -35,9 +41,76 @@ class Schwarzman(NyplUtils):
         # asserting title
         self.assert_title("Stephen A. Schwarzman Building | The New York Public Library")
 
-        # assert that clicking on 'directions' and '202x holiday hours' will open the correct page
-        # using 'text assertion' method from utility.py
-        self.text_assertion(SchwarzmanPage.directions, "google.com/maps")
-        self.text_assertion(SchwarzmanPage.holiday_closings, "nypl.org/help/closings")
-        self.text_assertion(SchwarzmanPage.research, "nypl.org/locations/schwarzman/research")
-        self.text_assertion(SchwarzmanPage.learn_more_link, "nypl.org/locations/schwarzman/research")
+        # assert that clicking on 'directions' and '202x holiday hours' will open the correct pages
+        # using 'link_assertion' method from utility.py
+        self.link_assertion(SchwarzmanPage.directions, "google.com/maps")
+        self.link_assertion(SchwarzmanPage.holiday_closings, "nypl.org/help/closings")
+        self.link_assertion(SchwarzmanPage.research, "nypl.org/locations/schwarzman/research")
+        self.link_assertion(SchwarzmanPage.learn_more_1, "nypl.org/locations/schwarzman/research")
+        self.link_assertion(SchwarzmanPage.learn_more_2, "nypl.org/spotlight/treasures")
+        self.link_assertion(SchwarzmanPage.daily_guided_tours, "nypl.org/events/tours/schwarzman")
+
+        # next spotlight and featured h3 content don't change often, therefore, full endpoints being asserted
+        self.link_assertion(SchwarzmanPage.in_the_spotlight_1, "nypl.org/appointments/schwarzman")
+        self.link_assertion(SchwarzmanPage.in_the_spotlight_2, "databases")
+
+        self.link_assertion(SchwarzmanPage.featured_at_sasb_1, "nypl.org/locations/schwarzman/research")
+        self.link_assertion(SchwarzmanPage.featured_at_sasb_2, "nypl.org/about/locations/schwarzman/shop-cafe")
+        self.link_assertion(SchwarzmanPage.featured_at_sasb_3, "www.nypl.org/blog")
+
+        # Exhibitions assertions are for dynamic content, hence, only base URL '/events/exhibitions' used for assertion
+
+        # write an addition to the utility. It will take the length of the list with
+        # '//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[1]/div/div/div[2]/ul/li'
+        # and then use this in a loop with a range and loop over the below statements.
+
+        self.link_assertion(SchwarzmanPage.current_exhibitions_1, "nypl.org/events/exhibitions")
+        self.link_assertion(SchwarzmanPage.current_exhibitions_2, "nypl.org/events/exhibitions")
+        self.link_assertion(SchwarzmanPage.current_exhibitions_3, "nypl.org/events/exhibitions")
+        self.link_assertion(SchwarzmanPage.current_exhibitions_4, "nypl.org/events/exhibitions")
+
+        # asserting 'Events - See All' web element
+        self.assert_element(SchwarzmanPage.events_see_all)
+        # asserting 'Events' with a for loop by clicking every event and asserting the title
+        # getting the length of the events h3 to use it in the for loop
+        h3_length = len(
+            self.find_elements('//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[2]/div/div/ul/li'))
+        # for loop to go over every event
+        for x in range(1, h3_length + 1):
+            # getting the link text and assert if it is in the page title
+            h3_link_text = self.get_text(
+                f'//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[2]/div/div/ul/li[{x}]/div[2]/h3/a')
+            print("\n1: " + h3_link_text)
+            self.click(f'//*[@id="block-nypl-emulsify-content"]/div/div/div[6]/div[2]/div/div/ul/li[{x}]/div[2]/h3')
+
+            # getting the page title element
+            h1_title = self.get_text('//*[@id="page-title"]')
+            print("2: " + h1_title)
+            # asserting h3 link text to the page title
+            self.assert_true(h3_link_text in h1_title)
+
+            self.go_back()  # go to the previous page for the next loop
+
+        # asserting 'About the Stephen A. ....'
+        self.assert_true(self.get_text(SchwarzmanPage.about_the_sasb) == "About the Stephen A. Schwarzman Building")
+
+    def test_sample21(self):
+
+        self.open("https://the-internet.herokuapp.com/broken_images")
+
+        self.image_assertion()
+
+    def test_sample_links(self):
+
+        # current exhibition list length to use in the for loop, this is dynamic, range from 1-4
+        link_amount = len(self.find_elements(self.current_exhibitions_list))
+
+        # current exhibition links to be asserted, depending on the link_amount above
+        link_elements = [SchwarzmanPage.current_exhibitions_1, SchwarzmanPage.current_exhibitions_2,
+                         SchwarzmanPage.current_exhibitions_3, SchwarzmanPage.current_exhibitions_4]
+
+        print(link_amount)  # optional print of the exhibition amount
+
+        for i in range(link_amount):
+            link = link_elements[i]
+            self.link_assertion(link, "nypl.org/events/exhibitions")
