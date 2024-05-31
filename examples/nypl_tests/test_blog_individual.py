c67b259273dd98ae25e22c78bcf8b6eb75628687
alkimcevik <alkimcevik@nypl.org>
2024-05-31 15:18:42 -0400

    section fronts kids & adults pages added

--- examples/nypl_tests/test_blog_individual.py
+++ examples/nypl_tests/test_blog_individual.py
@@ -1,3 +1,5 @@
+import pytest
+
 from examples.nypl_utility.utility import NyplUtils
 from examples.nypl_pages.page_blog_individual import BlogIndividualPage
 import random
@@ -34,19 +36,19 @@ class BlogIndividualTest(NyplUtils):
         self.assert_element(BlogIndividualPage.blog)
 
         # assert that page h3 link amount >= 1
-        page_link_number = len(self.find_elements(BlogIndividualPage.page_link_amount))
+        page_link_amount = len(self.find_elements(BlogIndividualPage.page_link_amount))
         # print(page_link_number)  # optional print
-        self.assert_true(page_link_number >= 1, "no h3 links on the page")
+        self.assert_true(page_link_amount >= 1, "no h3 links on the page")
 
         # assert that links on the page go to 'nypl.vega'
-        random_amount = 15  # random choice of amount of links to be clicked. 15 total links and random, as of Aug 2023
-        elements = list(range(1, page_link_number + 1))  # total range of links on the page
+        random_amount = page_link_amount  # random choice of amount of links to be clicked. could be changed for a faster run
+        elements = list(range(1, page_link_amount + 1))  # total range of links on the page
         random_elements = random.sample(elements, random_amount)  # random_amount to be tested out of total links
 
         # for loop to test random amount of links out of total links
         # as of Aug 2023, looping through all 15 links
         for x in random_elements:
-            link = f'(//*[@id="main-content"]//li//h3//a)[{x}]'
+            link = BlogIndividualPage.page_link_amount + f'[{x}]'
             self.click(link)
             current_url = self.get_current_url()
             print(f"\n {x}: " + current_url)
