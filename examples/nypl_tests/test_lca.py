530f859486cf2c26086b26b7658ad87493de6f35
alkimcevik <alkimcevik@nypl.org>
2025-06-04 18:01:28 -0400

    lca test updated

--- examples/nypl_tests/test_lca.py
+++ examples/nypl_tests/test_lca.py
@@ -15,6 +15,7 @@ load_dotenv()
 
 # @pytest.mark.test
 # @pytest.mark.skip
+@pytest.mark.only_firefox
 @pytest.mark.smoke
 class LibraryCard(NyplUtils):
     # https://www.nypl.org/library-card/new
@@ -58,7 +59,6 @@ class LibraryCard(NyplUtils):
             self.assert_element(LibraryCardPage.previous_button)
             self.click(LibraryCardPage.next_button)
 
-    @pytest.mark.only_firefox
     @pytest.mark.smoke
     def test_library_card_new(self):
 
@@ -173,7 +173,6 @@ class LibraryCard(NyplUtils):
 
         print(self.get_current_url())
 
-        # self.wait(2455)
         # todo: update the new home library dropdown
         # self.send_keys(LibraryCardPage.home_library_box, "Stephen A. Schwarzman Building")
         # self.send_keys(LibraryCardPage.home_library_box, Keys.ENTER)
