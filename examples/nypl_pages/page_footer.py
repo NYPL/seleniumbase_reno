6e9f387a94fa26fb1f83d14811a915a19df16ab2
alkimcevik <alkimcevik@nypl.org>
2026-02-01 12:23:08 -0500

    footer locator updates

--- examples/nypl_pages/page_footer.py
+++ examples/nypl_pages/page_footer.py
@@ -2,19 +2,19 @@ from seleniumbase import BaseCase
 
 
 class FooterPage(BaseCase):
-    accessibility = '//*[@id="nypl-footer"]//*[contains(text(), "Accessibility")]'
-    press = '//*[@id="nypl-footer"]//*[contains(text(), "Press")]'
-    careers = '//*[@id="nypl-footer"]//*[contains(text(), "Careers")]'
-    space_rental = '//*[@id="nypl-footer"]//*[contains(text(), "Space Rental")]'
+    accessibility = '//*[@id="nypl-footer"]//a[contains(text(), "Accessibility")]'
+    press = '//*[@id="nypl-footer"]//a[contains(text(), "Press")]'
+    careers = '//*[@id="nypl-footer"]//a[contains(text(), "Careers")]'
+    space_rental = '//*[@id="nypl-footer"]//a[contains(text(), "Space Rental")]'
 
-    privacy_policy = '//*[@id="nypl-footer"]//*[contains(text(), "Privacy Policy")]'
-    other_policies = '//*[@id="nypl-footer"]//*[contains(text(), "Other Policies")]'
-    terms_conditions = '//*[@id="nypl-footer"]//*[contains(text(), "Terms & Conditions")]'
-    governance = '//*[@id="nypl-footer"]//*[contains(text(), "Governance")]'
+    privacy_policy = '//*[@id="nypl-footer"]//a[contains(text(), "Privacy Policy")]'
+    other_policies = '//*[@id="nypl-footer"]//a[contains(text(), "Other Policies")]'
+    terms_conditions = '//*[@id="nypl-footer"]//a[contains(text(), "Terms & Conditions")]'
+    governance = '//*[@id="nypl-footer"]//a[contains(text(), "Governance")]'
 
-    rules_regulations = '//*[@id="nypl-footer"]//*[contains(text(), "Rules & Regulations")]'
-    about_nypl = '//*[@id="nypl-footer"]//*[contains(text(), "About NYPL")]'
-    language = '//*[@id="nypl-footer"]//*[contains(text(), "Language")]'
+    rules_regulations = '//*[@id="nypl-footer"]//a[contains(text(), "Rules & Regulations")]'
+    about_nypl = '//*[@id="nypl-footer"]//a[contains(text(), "About NYPL")]'
+    language = '//*[@id="nypl-footer"]//a[contains(text(), "Language")]'
 
     facebook = '(//*[@data-testid="social-media-links"]//li)[1]//a'
     twitter = '(//*[@data-testid="social-media-links"]//li)[2]//a'
