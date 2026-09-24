a8e25a5639818fd24a89248018715cb425d62b39
alkimcevik <alkimcevik@nypl.org>
2026-09-23 22:13:39 -0400

    test: intentionally break header smoke test to verify Slack notification
    TEMPORARY. Revert after confirming the Prod Smoke Slack alert fires
    following the org-level Slack app name change.

--- examples/nypl_tests/test_header.py
+++ examples/nypl_tests/test_header.py
@@ -80,7 +80,8 @@ class HeaderTest(NyplUtils):
 
         # assert Search
         self.click(HeaderPage.search_button)
-        self.assert_text("Close")
+        # TEMP: intentional failure to verify Slack notification. Revert after test.
+        self.assert_text("INTENTIONAL_FAILURE_SLACK_NOTIFICATION_CHECK")
 
     # @pytest.mark.skip(reason="Chris covering this in his RC automation suite")
     @pytest.mark.smoke
